4cd8a6cdae5cae54f3b6c888e9c6d2897ed65c49
devmk1090 <devmk1090@gmail.com>
2023-07-24 01:17:08 +0900

    change project name

--- src/main/java/com/devk/srping/basic/SpringBasicApplication.java
+++ src/main/java/com/devk/srping/basic/SpringBasicApplication.java
@@ -1,13 +1,13 @@
-package com.devk.crud.crudexample;
+package com.devk.srping.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CrudExampleApplication {
+public class SpringBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudExampleApplication.class, args);
+		SpringApplication.run(SpringBasicApplication.class, args);
 	}
 
 }
--- src/test/java/com/devk/srping/basic/SpringBasicApplicationTests.java
+++ src/test/java/com/devk/srping/basic/SpringBasicApplicationTests.java
@@ -1,10 +1,10 @@
-package com.devk.crud.crudexample;
+package com.devk.srping.basic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CrudExampleApplicationTests {
+class SpringBasicApplicationTests {
 
 	@Test
 	void contextLoads() {
