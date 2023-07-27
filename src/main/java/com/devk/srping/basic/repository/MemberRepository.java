e7de151201e26cd12a58bc819891487203815702
devmk1090 <devmk1090@gmail.com>
2023-07-28 02:58:51 +0900

    - 회원 도메인과 리포지터리 구현 - 회원 리포지터리 테스트 케이스 구현 테스트 메서드는 각자 따로 돌아가기 떄문에 @AfterEach 어노테이션을 붙인 메서드안에서 클리어를 해주자.

---
+++ src/main/java/com/devk/srping/basic/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.devk.srping.basic.repository;
+
+import com.devk.srping.basic.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
