e7245dce6604b1e2990bd05b78eca362b0c4a4de
devmk1090 <devmk1090@gmail.com>
2023-08-02 02:22:41 +0900

    - 회원 서비스 구현 - 회원 서비스 테스트 구현

---
+++ src/main/java/com/devk/srping/basic/service/MemberService.java
@@ -0,0 +1,45 @@
+package com.devk.srping.basic.service;
+
+import com.devk.srping.basic.domain.Member;
+import com.devk.srping.basic.repository.MemberRepository;
+import com.devk.srping.basic.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //여기서 new MemberRepository() 를 하지 않고 외부에서 생성하여 생성자에 넣어주자.(DI)
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
