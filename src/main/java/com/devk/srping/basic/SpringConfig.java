e3e5fd412e957e1a5813630e22135c6403d0f1f4
devmk1090 <devmk1090@gmail.com>
2023-08-12 01:37:01 +0900

    - Jdbc 종속성 추가 - 회원 서비스 스프링 통합 테스트 추가 스프링 컨테이너와 DB까지 연결한 통합 테스트 - JdbcTemplate 구현 기존 Jdbc의 반복 코드를 대부분 제거해주지만, SQL은 직접 작성해야 한다.

--- src/main/java/com/devk/srping/basic/SpringConfig.java
+++ src/main/java/com/devk/srping/basic/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.devk.srping.basic;
 
+import com.devk.srping.basic.repository.JdbcTemplateMemberRepository;
 import com.devk.srping.basic.repository.MemberRepository;
 import com.devk.srping.basic.repository.MemoryMemberRepository;
 import com.devk.srping.basic.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
